27426a0a04b82e9ad66a1570e24a0fef35835114
joelgrod <joelgrod@gmail.com>
2021-08-15 19:11:26 +0100

    answerList component, lifeCycle hooks and outputs

--- src/pages/PokeHomePage.vue
+++ src/pages/PokeHomePage.vue
@@ -1,30 +1,54 @@
 <template>
+  <div v-if="!pokemon">
+      <h1>Wait a moment please...</h1>
+  </div>
+  <div v-else>
     <h1>Recognize the Pokemon</h1>
-    <PokemonImg :imgId="100" :showPokemon="true"></PokemonImg>
-    <AnswersList></AnswersList>
+    <PokemonImg :img-id="pokemon.id" 
+        :show-pokemon="showPokemon">
+    </PokemonImg>
+    <AnswersList :pokemons="pokemons" 
+        @selection-pokemon="checkPokemon($event)">
+    </AnswersList>
+  </div>
 </template>
 
 <script>
-    // Components
-    import AnswersList from '@/components/AnswersList'
-    import PokemonImg from '@/components/PokemonImg'
-    // Helpers
-    import getPokemonOptions from '@/helpers/getPokemonOptions'
+// Components
+import AnswersList from "@/components/AnswersList";
+import PokemonImg from "@/components/PokemonImg";
+// Helpers
+import getPokemonOptions from "@/helpers/getPokemonOptions";
 
 export default {
-    name: 'PokeHomePage',
-    computed: {
-        getPokemonsInfo() {
-            return getPokemonOptions()
-        }
+  name: "PokeHomePage",
+  data() {
+    return {
+      pokemons: [],
+      pokemon: null,
+      showPokemon: false
+    };
+  },
+  methods: {
+    async pokemonsList() {
+      this.pokemons = await getPokemonOptions();
+      const rndInt = Math.floor(Math.random() * 4);
+      this.pokemon = this.pokemons[rndInt];
     },
-    components: {
-        AnswersList,
-        PokemonImg
+    checkPokemon(pokemonId) {
+        this.showPokemon = (pokemonId === this.pokemon.id) ? true : false
     }
-}
+  },
+  components: {
+    AnswersList,
+    PokemonImg,
+  },
+  // LifeCycle
+  mounted() {
+    this.pokemonsList();
+  },
+};
 </script>
 
 <style>
-
 </style>
