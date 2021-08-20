15adb18dee98e6d30fc5849dfcafb6b100629b4b
joelgrod <joelgrod@gmail.com>
2021-08-20 22:52:16 +0100

    PokemonImg component tests added

--- tests/unit/components/PokemonImg.spec.js
+++ tests/unit/components/PokemonImg.spec.js
@@ -29,9 +29,25 @@ describe('PokemonImg.vue', () => {
         expect(img1.exists()).toBeTruthy();
         expect(img2).toBe(undefined);
         expect(img1.classes('hidden-pokemon')).toBeTruthy();
+        expect(img1.attributes().src)
+            .toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg');
     });
 
     test('should show pokemon if showpicture is true', () => {
-        
+        // Arrange
+        const wrapper = shallowMount(PokemonImg, {
+            props: {
+                imgId: 100,
+                showPokemon: true
+            }
+        });
+        // Act
+        const img = wrapper.find('img');
+        // Assert
+        expect(img.exists()).toBeTruthy();
+        expect(img.classes('fade-in')).toBeTruthy();
+        expect(img.classes('hidden-pokemon')).toBeFalsy();
+        expect(wrapper.vm.getImgSrc)
+            .toBe('https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/100.svg');
     });
 });
