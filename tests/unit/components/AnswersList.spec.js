b8035e4c31552aad94451176fa074a283000bb29
joelgrod <joelgrod@gmail.com>
2021-08-22 11:44:19 +0100

    AnswersList components tests added

--- tests/unit/components/AnswersList.spec.js
+++ tests/unit/components/AnswersList.spec.js
@@ -32,4 +32,34 @@ describe("AnswersList Component", () => {
       </div>
     `);
   });
+
+  test('should show the four options correctly', () => {
+    // Arrange
+    const listItems = wrapper.findAll('li');
+    // Act
+    // Assert
+    expect(listItems.length).toBe(4);
+    expect(listItems[0].text()).toBe('bulbasaur');
+    expect(listItems[1].text()).toBe('ivysaur');
+    expect(listItems[2].text()).toBe('venusaur');
+    expect(listItems[3].text()).toBe('charmander');
+    
+  });
+
+  test('should emit \'selection-pokemon\n with the correct id when clicked', () => {
+    // Arrange
+    const [ li1, li2, li3, li4 ] = wrapper.findAll( 'li' );
+    // Act
+    li1.trigger('click');
+    li2.trigger('click');
+    li3.trigger('click');
+    li4.trigger('click');
+    // Assert
+    expect( wrapper.emitted('selection-pokemon').length ).toBe(4);
+    expect( wrapper.emitted('selection-pokemon')[0] ).toEqual([1]);
+    expect( wrapper.emitted('selection-pokemon')[1] ).toEqual([2]);
+    expect( wrapper.emitted('selection-pokemon')[2] ).toEqual([3]);
+    expect( wrapper.emitted('selection-pokemon')[3] ).toEqual([4]);
+  });
+  
 });
